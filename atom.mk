879fb2229adf254d62e4c6d0dc462f1384614a4a
Parthiban Nallathambi <parthiban.nallathambi@pantacor.com>
2022-11-21 13:43:08 +0530

    add atom.mk for pantavisor build
    Signed-off-by: Parthiban Nallathambi <parthiban.nallathambi@pantacor.com>

---
+++ atom.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libkcapi
+LOCAL_DESCRIPTION := libkcapi
+
+LOCAL_EXPORT_LDLIBS =
+
+LOCAL_AUTOTOOLS_VERSION := 2.0.4
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS := \
+	--enable-kcapi-encapp \
+	CFLAGS=-Wconversion \
+	$(NULL)
+
+LOCAL_AUTOTOOLS_CONFIGURE_ENV := \
+	$(NULL)
+
+include $(BUILD_AUTOTOOLS)
